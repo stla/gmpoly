ab794d24389be13cbe2afbd02de6e508d424faec
stla <laurent_step@yahoo.fr>
2022-02-17 18:49:42 +0100

    README.Rmd

--- inst/essais/essais_gmpoly.R
+++ inst/essais/essais_gmpoly.R
@@ -13,4 +13,18 @@ pol2 <- gmpoly("-2 x^(1,1) + 3 x^(2,1)")
 pol1 + pol2
 pol1 * pol2
 
+pol <- gmpoly("4 x^(2) + 1/2 x^(1)")
++pol
+-pol
+2 * pol
+pol / 2
+pol + 5
+pol - 5
+pol^2
+pol1 <- gmpoly("2 x^(1) - 5/3 x^(2)")
+pol2 <- gmpoly("-2 x^(1) + 3 x^(2)")
+pol1 + pol2
+pol1 * pol2
+
+
 polAsString(stringToPol("4 x^(3,4) + 2"))
